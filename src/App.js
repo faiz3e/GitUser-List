dd014c7b3b323d4f627e13fd4c89b8605502a53f
Faizan shaikh <32793214+faiz3e@users.noreply.github.com>
2018-06-15 13:29:52 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-// import TimeAgo from 'javascript-time-ago'
 import './App.css';
+import DisplayList from './containers/DisplayList';
 
 class App extends Component {
   constructor(props) {
@@ -8,69 +8,40 @@ class App extends Component {
     this.state = {
       nameToSearch: "",
       arrayData: [],
-      showMoreToggle: false,
-      additionalData: [],
       selectOptionFilter: '',
     }
+    this.sortByEvent=this.sortByEvent.bind(this);
   }
-
+  
   showData = () => {
     this.getUserData();
   }
 
-
   getUserData() {
-    var nameToSearch = this.state.nameToSearch;
-    if (nameToSearch)
-      fetch(' https://api.github.com/search/users?q=' + nameToSearch)
+    this.state.nameToSearch ?
+      fetch(' https://api.github.com/search/users?q=' + this.state.nameToSearch)
         .then(response => response.json())
-        .then(arrayData => this.setState({ arrayData }, function () { console.log("data >>>", this.state.arrayData, " length of items", Object.keys(this.state.arrayData.items).length) }))
+        .then(arrayData => this.setState({ arrayData }, function () {console.log("data>",this.state.arrayData)}))
         .catch(e => e)
-    else { console.log("enter some value") }
+    : console.log("enter some value") 
   }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value })
-    if ((event.target.name !== 'nameToSearch') && (this.state.arrayData.length !== 0) && (this.state.selectOptionFilter !== ''))
-      this.sortByEvent(event.target.value)
   }
 
   sortByEvent(e) {
-    var newSortedArray = this.state.arrayData.items;
-    switch (e) {
-      case "Name_asc":
-        newSortedArray.sort(function (a, b) { return a.login - b.login });
-        break;
-      case "Name_dec":
-        newSortedArray.sort(function (a, b) { return b.login - a.login });
-        break;
-      case "Rank_asc":
-        newSortedArray.sort(function (a, b) { return a.score - b.score });
-        break;
-      case "Rank_dec":
-        newSortedArray.sort(function (a, b) { return b.score - a.score });
-        break;
-      default:
-        break;
+    if(this.state.arrayData){
+      var newSortedArray = this.state.arrayData.items;
+      var filterBy= JSON.parse(e.target.value);
+      newSortedArray.sort(function (a, b) {  return filterBy.order==="asc"?  a[filterBy.key] - b[filterBy.key] : b[filterBy.key] - a[filterBy.key] });
+      this.setState({
+      newSortedArray:newSortedArray
+      })
     }
   }
 
-  showmore(id, url) {
-    if (this.state.showMoreToggle === id) { id = "" }
-    this.setState({ showMoreToggle: id }, function () {
-      console.log("button on ", this.state.showMoreToggle);
-      this.fetchMore(url);
 
-    })
-
-  }
-  fetchMore(url) {
-    console.log("url", url);
-    fetch(url)
-      .then(response => response.json())
-      .then(additionalData => this.setState({ additionalData }, function () { console.log("additional data >>>", this.state.additionalData) }))
-      .catch(e => e)
-  }
 
   render() {
     const posts = this.state.arrayData.items;
@@ -81,47 +52,17 @@ class App extends Component {
           <div>
             <span>Filter &#x21C5;</span>
 
-            <select name="selectOptionFilter" value={this.state.selectOptionFilter} onChange={this.handleChange.bind(this)}>
-              <option value="Name_asc">Name &#8613;</option>
-              <option value="Name_dec">Name &#8615;</option>
-              <option value="Rank_asc">Rank &#8613;</option>
-              <option value="Rank_dec">Rank &#8615;</option>
+            <select name="selectOptionFilter" value={this.state.selectOptionFilter.key} onChange={this.sortByEvent.bind(this)}>
+              <option value='{ "key":"login", "order":"asc"}'>Name &#8613;</option>
+              <option value='{ "key":"login", "order":"dec"}'>Name &#8615;</option>
+              <option value='{ "key":"score", "order":"asc"}'>Rank &#8613;</option>
+              <option value='{ "key":"score", "order":"dec"}'>Rank &#8615;</option>
             </select><br />
             <button onClick={() => this.showData()}>show data</button>
           </div>
         </div>
+        {posts && <DisplayList post={posts} arrayDataCount={this.state.arrayData.total_count} /> }
 
-        {posts &&
-          <div>
-            <div>
-              <p>No of Records showing:{Object.keys(posts).length} of {this.state.arrayData.total_count} </p>
-            </div>
-            <ul className="ul-style">
-              {posts.map((post) =>
-                <li className="w3-card-4 li-style" style={{width:"30%" , marginBottom:"20px"}} key={post.id}>
-                <header class="w3-container w3-light-blue" >
-                  <h4 style={{color:"white" }}>{post.login}</h4>
-                </header>
-                <div class="w3-container"   style={{marginTop:"10px"}}>
-                  <img className="w3-left w3-circle w3-margin-right" src={post.avatar_url} alt="avatar" style={{width:"60px"}} />
-                  <a href={post.html_url} className="title">{post.html_url}</a>
-                  <p>Score:{post.score}</p>
-                  
-                   
-                  </div>
-                  <button className="w3-button w3-block" onClick={this.showmore.bind(this, post.id, post.url)}>
-                      {this.state.showMoreToggle === post.id ? <span>Show less</span> : <span>Show more</span>}</button>
-                
-                  {this.state.showMoreToggle === post.id &&
-                    <div className="additional-style">
-                      <p>created at: {this.state.additionalData.created_at}</p>
-                      <p>followers: {this.state.additionalData.followers}</p>
-                      <p>public repos: {this.state.additionalData.public_repos}</p>
-                      <p>{this.state.additionalData.location}</p>
-                    </div>}
-                </li>)}
-            </ul>
-          </div>}
       </div>
     );
   }
